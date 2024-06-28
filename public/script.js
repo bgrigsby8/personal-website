29bd0b0cc02a6609963db6cdb619265f140500c5
Brad Grigsby <bgrigsby@righthandrobotics.com>
2024-06-28 00:20:44 -0400

    Updated gitignore and animations

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ function playAnimation(videoFile, lastFrameId) {
 
     video.src = videoFile;
     video.setAttribute('data-last-video', videoFile);
+    video.setAttribute('last-frame-id', lastFrameId);
     video.style.display = 'block'; // Show video
     document.querySelector('button').style.display = 'block'; // Show back button
     video.play();
@@ -21,6 +22,9 @@ function reverseAnimation() {
     const video = document.getElementById('animationVideo');
     const allLastFrames = document.querySelectorAll('[id^="lastFrame"]');
 
+    let lastFrameId = video.getAttribute('last-frame-id');
+    const lastFrame = document.getElementById(lastFrameId)
+
     let lastVideo = video.getAttribute('data-last-video');
     let reversedVideo = lastVideo.replace('.mp4', '_reversed.mp4')
 
@@ -29,8 +33,12 @@ function reverseAnimation() {
     video.play();
 
     video.onended = function() {
+        lastFrame.style.display = 'none';
         video.style.display = 'none';  // Hide video
+        console.log("Made last frame and video display none")
         document.getElementById('firstFrame').style.display = 'block';  // Show the first frame image again
+        console.log("Made first frame active")
         document.querySelector('button').style.display = 'none';  // Hide back button
+        console.log("Made button none")
     };
 }
